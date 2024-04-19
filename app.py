6fd1d9955bf3a980d7acae7d4facdd0df7064b91
Louis Mazin <mazinlouis79@gmail.com>
2024-04-19 13:40:01 +0200

    Initialisation

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route('/getWays')
 def getWays():
-    if not request.args.get('number') == None:
+    if request.args.get('number') != None:
         return Graph.getJSONShortestWays(request.args.get('parent'), request.args.get('child'),int(request.args.get('number')))
