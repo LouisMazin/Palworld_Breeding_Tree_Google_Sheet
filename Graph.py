10b0454a142ab000e6ea6b4300086aac8ecef6e9
Louis Mazin <mazinlouis79@gmail.com>
2024-04-19 19:53:05 +0200

    init

--- Graph.py
+++ Graph.py
@@ -185,7 +185,7 @@ def getJSONShortestWays(parent : str, child : str,number : int):
     way=ways[number-1]
     res=[]
     for palIndex in range(len(way)-1):
-        res.append(way[palIndex],findParents(way[palIndex],way[palIndex+1]))
+        res.append([way[palIndex],findParents(way[palIndex],way[palIndex+1])])
     res[way[-1]]=[]
     jsonways={"way":res,"length":len(ways)}
     return jsonways
