40e6eba359fc0809df76b28e285a358cfa819e94
Louis Mazin <mazinlouis79@gmail.com>
2024-04-19 17:33:19 +0200

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import Graph,os
 app = Flask(__name__)
 @app.route('/')
 def index():
-    os.system("sudo apt install graphviz")
+    os.system("apt install graphviz")
 @app.route('/getWays')
 def getWays():
     return Graph.getJSONShortestWays(request.args.get('parent'), request.args.get('child'),int(request.args.get('number')))
