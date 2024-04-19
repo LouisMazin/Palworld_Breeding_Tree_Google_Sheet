e92b8fa8ffc76c4aca793dae095d2f541add28d2
Louis Mazin <mazinlouis79@gmail.com>
2024-04-19 16:27:46 +0200

    test

--- app.py
+++ app.py
@@ -5,5 +5,3 @@ app = Flask(__name__)
 @app.route('/getWays')
 def getWays():
     return Graph.getJSONShortestWays(request.args.get('parent'), request.args.get('child'),int(request.args.get('number')))
-
-print(Graph.getJSONShortestWays("Frostallion","Lamball",1))
