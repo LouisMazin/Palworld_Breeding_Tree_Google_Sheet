905c362d379da8c624c10d767e72d18f188dc077
Louis Mazin <mazinlouis79@gmail.com>
2024-04-19 15:16:59 +0200

    Initialisation

--- Graph.py
+++ Graph.py
@@ -184,5 +184,5 @@ def getShortestWays(parent : str, child : str):
 
 def getJSONShortestWays(parent : str, child : str,number : int):
     ways=getShortestWays(parent,child)
-    jsonways=[len(ways)]+ways[number-1]
+    jsonways={"way":ways[number-1],"length":len(ways)}
     return jsonways
