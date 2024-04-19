993f2a52cef547e871394066fdf33816dd1d9627
Louis Mazin <mazinlouis79@gmail.com>
2024-04-19 13:09:31 +0200

    test

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 
 @app.route('/<string:source>/<string:destination>')
 def start(*args):
+    if(len(args) != 2):
+        return "Invalid number of arguments"
     return Graph.getJSONShortestWays(args[0], args[1])
