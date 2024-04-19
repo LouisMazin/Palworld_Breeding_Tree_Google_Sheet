4470dafa346f6652667db296294cd5e25339b3d4
Louis Mazin <mazinlouis79@gmail.com>
2024-04-19 17:19:33 +0200

    test

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 from flask import Flask, request
 import Graph,os
 app = Flask(__name__)
-@app.route('/')
-def index():
-    os.system('wget https://gitlab.com/graphviz/graphviz/-/package_files/6163716/download')
-    os.system('tar -xf download')
-    os.system('rm download')
-    os.system('mv graphviz-2.46.0 graphviz')
-    os.system('cd graphviz && ./configure && make && make install')
-    os.system('export PATH=$PATH:/usr/local/bin/graphviz/dot')
 @app.route('/getWays')
 def getWays():
     return Graph.getJSONShortestWays(request.args.get('parent'), request.args.get('child'),int(request.args.get('number')))
