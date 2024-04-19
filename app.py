adfa129e8bfa8293efb221f6fb8e4f09fed55c82
Louis Mazin <mazinlouis79@gmail.com>
2024-04-19 13:08:23 +0200

    test

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 import Graph
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/<string:source>/<string:destination>')
 def start(*args):
     return Graph.getJSONShortestWays(args[0], args[1])
