8831861bfecc561968f310e97bb32b06e30342cb
Louis Mazin <mazinlouis79@gmail.com>
2024-10-15 01:23:44 +0200

    Update for self hosting

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
 from flask import Flask, request
 import Graph
-
-import os
-import subprocess
-
-def obtain_ssl_certificate(domain):
-    # Command to run Certbot
-    command = [
-        'certbot', 'certonly', '--standalone',
-        '--non-interactive', '--agree-tos',
-        '-m mazinlouis79@gmail.com',  # Replace with your email
-        '-d', domain
-    ]
-    
-    try:
-        # Run the command to obtain the SSL certificate
-        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("SSL certificate obtained successfully!")
-    except subprocess.CalledProcessError as e:
-        print("Error obtaining SSL certificate:")
-        print(e.stderr.decode())
-
-# Call the function with your domain name
-obtain_ssl_certificate('panel.louismazin.com')
-
 app = Flask(__name__)
-@app.route('/getWays')
+app.logger.disabled = True
+@app.route('/getWays', methods=['GET'])
 def getWays():
     return Graph.getJSONShortestWays(request.args.get('parent'), request.args.get('child'),int(request.args.get('number')))
+
+app.run(host='0.0.0.0', port=1029)
