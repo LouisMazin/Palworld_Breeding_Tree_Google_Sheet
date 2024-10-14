136c861104c2d7352fc5da07bccc6959785044c9
Louis Mazin <mazinlouis79@gmail.com>
2024-10-15 00:36:39 +0200

    test

--- app.py
+++ app.py
@@ -4,4 +4,3 @@ app = Flask(__name__)
 @app.route('/getWays')
 def getWays():
     return Graph.getJSONShortestWays(request.args.get('parent'), request.args.get('child'),int(request.args.get('number')))
-app.run(host='77.207.48.109',port=1029)
