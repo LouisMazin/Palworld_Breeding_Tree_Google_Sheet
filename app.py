c5c2d9eb3174cf1cb5dadd5c5d224b94ce40e173
Louis Mazin <mazinlouis79@gmail.com>
2024-10-15 00:25:13 +0200

    test

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def getWays():
     return Graph.getJSONShortestWays(request.args.get('parent'), request.args.get('child'),int(request.args.get('number')))
 
 if __name__ == '__main__': 
-    app.run(host='192.168.0.105') 
+    app.run(host='77.207.48.109',port=1029) 
