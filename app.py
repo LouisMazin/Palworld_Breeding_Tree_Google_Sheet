e42b059d3acfcd8a714c7d224f753514c5a30860
Louis Mazin <mazinlouis79@gmail.com>
2024-10-15 04:03:55 +0200

    update

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request
 import Graph
 app = Flask(__name__)
-app.logger.disabled = True
+
 @app.route('/getWays', methods=['GET'])
 def getWays():
+    print("getWays : ", request.args.to_dict())
     return Graph.getJSONShortestWays(request.args.get('parent'), request.args.get('child'),int(request.args.get('number')))
-
-app.run(host='0.0.0.0', port=1029)
